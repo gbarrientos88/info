459d366170baa82332925134beb6a9d0fe53542f
German Barrientos <gbarrientos88@gmail.com>
2014-07-02 15:55:53 -0400

    primer version

---
+++ logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-06-15 04:40:11 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2014-06-15	04:40:11	INFO	127.0.0.1	Joomla FAILURE: 	Contraseña inválida
+2014-06-15	05:06:08	INFO	127.0.0.1	Joomla FAILURE: 	Contraseña vacia no permitida.
+2014-06-15	05:06:09	INFO	127.0.0.1	Joomla FAILURE: 	Contraseña vacia no permitida.
+2014-06-15	05:06:15	INFO	127.0.0.1	Joomla FAILURE: 	Contraseña inválida
+2014-07-01	15:51:41	INFO	127.0.0.1	Joomla FAILURE: 	Contraseña inválida
